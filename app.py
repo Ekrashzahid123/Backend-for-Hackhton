0ac3fc404261517976eb045fd0d331011d724770
Abrar Tahir <abrartahir789@gmail.com>
2026-07-27 22:07:08 +0500

    Create app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 import threading
+import os
 
 from database.database import Base, engine
 from routes import upload, generate, search
